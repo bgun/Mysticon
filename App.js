7738e17470c6636da89d88ad2e2c0e29828f63b8
Ben Gundersen <ben@bengundersen.com>
2018-02-05 22:09:23 -0500

    upgrading to new react-native version

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import MainView from './views/MainView';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <MainView />
+    );
+  }
+}
